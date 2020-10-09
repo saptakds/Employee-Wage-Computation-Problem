988c998099103a95d0234e44699e3cceccb0bf46
saptakds <saptakds@gmail.com>
2020-10-09 17:44:59 +0530

    Added: Calculated Wages for a month

--- src/com/saptak/employeewage/EmployeeWageProblem.java
+++ src/com/saptak/employeewage/EmployeeWageProblem.java
@@ -8,6 +8,7 @@ public class EmployeeWageProblem {
 	private static final int WAGE_PER_HOUR = 20;
 	private static final int DAILY_WAGE = FULL_DAY_WORKING_HOURS * WAGE_PER_HOUR;
 	private static final int PART_TIME_WORKING_HOURS = 8;
+	private static final int WORKING_DAYS_PER_MONTH = 20;
 
 	public static void main(String[] args) {
 		System.out.println("-->Welcome to Employee Wage Computation<--");
@@ -21,11 +22,22 @@ public class EmployeeWageProblem {
 		default:
 			break;
 		}
-		if (isEmployeePresent()) {
-			System.out.println("Employee is PRESENT");
-		} else {
-			System.out.println("Employee is ABSENT");
+		System.out.println("Daily wage: " + DAILY_WAGE);
+		System.out.println("Wages for month: " + getWagesForMonth());
+	}
+
+	private static long getWagesForMonth() {
+		long wages = 0;
+		for (int i = 1; i <= WORKING_DAYS_PER_MONTH; i++) {
+			System.out.println("Day #" + i);
+			if (isEmployeePresent()) {
+				System.out.println("Present");
+				wages += DAILY_WAGE;
+			} else {
+				System.out.println("Absent");
+			}
 		}
+		return wages;
 	}
 
 	private static boolean isEmployeePresent() {
