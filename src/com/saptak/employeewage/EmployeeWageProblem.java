351709d6e4639275e23aab9cff337188b31b6389
saptakds <saptakds@gmail.com>
2020-10-09 12:12:31 +0530

    Added: Display Welcome message

---
+++ src/com/saptak/employeewage/EmployeeWageProblem.java
@@ -0,0 +1,10 @@
+package com.saptak.employeewage;
+
+public class EmployeeWageProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("-->Welcome to Employee Wage Computation<--");
+	}
+
+}
