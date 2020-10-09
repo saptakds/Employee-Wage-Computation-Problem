0be6a6f437f8661a3b7f31b02f4bde39593c9fba
saptakds <saptakds@gmail.com>
2020-10-09 12:27:11 +0530

    Added: Check if employee is present or absent

--- src/com/saptak/employeewage/EmployeeWageProblem.java
+++ src/com/saptak/employeewage/EmployeeWageProblem.java
@@ -1,10 +1,20 @@
 package com.saptak.employeewage;
 
+import java.util.Random;
+
 public class EmployeeWageProblem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("-->Welcome to Employee Wage Computation<--");
+		if (isEmployeePresent()) {
+			System.out.println("Employee is PRESENT");
+		} else {
+			System.out.println("Employee is ABSENT");
+		}
 	}
 
+	public static boolean isEmployeePresent() {
+		return new Random().nextBoolean();
+	}
 }
