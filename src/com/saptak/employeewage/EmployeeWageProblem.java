ab72fe478b4824c1c4ae238fbacd377775634371
saptakds <saptakds@gmail.com>
2020-10-09 12:54:24 +0530

    Added: Part time employee & wage

--- src/com/saptak/employeewage/EmployeeWageProblem.java
+++ src/com/saptak/employeewage/EmployeeWageProblem.java
@@ -7,6 +7,7 @@ public class EmployeeWageProblem {
 	private static final int FULL_DAY_WORKING_HOURS = 8;
 	private static final int WAGE_PER_HOUR = 20;
 	private static final int DAILY_WAGE = FULL_DAY_WORKING_HOURS * WAGE_PER_HOUR;
+	private static final int PART_TIME_WORKING_HOURS = 8;
 
 	public static void main(String[] args) {
 		System.out.println("-->Welcome to Employee Wage Computation<--");
