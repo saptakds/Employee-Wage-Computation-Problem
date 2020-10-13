058374502dc40f6b04b02e71a3e3033f2e9b995b
saptakds <saptakds@gmail.com>
2020-10-13 17:36:39 +0530

    Added: Employee wage computation for multiple companies

--- src/com/saptak/employeewage/EmployeeWageProblem.java
+++ src/com/saptak/employeewage/EmployeeWageProblem.java
@@ -5,51 +5,47 @@ import java.util.Random;
 public class EmployeeWageProblem {
 
 	private static final int FULL_DAY_WORKING_HOURS = 8;
-	private static final int WAGE_PER_HOUR = 20;
-	private static final int DAILY_WAGE = FULL_DAY_WORKING_HOURS * WAGE_PER_HOUR;
-	private static final int PART_TIME_WORKING_HOURS = 8;
-	private static final int WORKING_DAYS_PER_MONTH = 20;
-	private static final int WORKING_HOURS_FOR_A_MONTH = 100;
+	private final String companyName;
+	private final int dailyWage;
+	private final int workingDaysPerMonth;
+	private final int workingHoursPerMonth;
 
-	public static void main(String[] args) {
-		System.out.println("-->Welcome to Employee Wage Computation<--");
-		switch (getEmployeeType()) {
-		case "Full-time":
-			System.out.println("Employment: Full-time");
-			break;
-		case "Part-time":
-			System.out.println("Employment: Part-time");
-			break;
-		default:
-			break;
-		}
-		System.out.println("Daily wage: " + DAILY_WAGE);
-		System.out.println("Wages for month: " + getWagesForMonth());
+	public EmployeeWageProblem(String companyName, int dailyWage, int workingDaysPerMonth, int workingHoursPerMonth) {
+		this.companyName = companyName;
+		this.dailyWage = dailyWage;
+		this.workingDaysPerMonth = workingDaysPerMonth;
+		this.workingHoursPerMonth = workingHoursPerMonth;
 	}
 
-	private static long getWagesForMonth() {
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public long getWagesForMonth() {
 		long wages = 0;
 		int workingHours = 0, workingDays = 0;
-		while (workingDays < WORKING_DAYS_PER_MONTH && workingHours < WORKING_HOURS_FOR_A_MONTH) {
+		while (workingDays < workingDaysPerMonth && workingHours < workingHoursPerMonth) {
 			workingDays++;
-			System.out.println("Day #" + workingDays);
 			if (isEmployeePresent()) {
-				System.out.println("Present");
 				workingHours += FULL_DAY_WORKING_HOURS;
-				wages += DAILY_WAGE;
-			} else {
-				System.out.println("Absent");
+				wages += dailyWage;
 			}
 		}
 		return wages;
 	}
 
-	private static boolean isEmployeePresent() {
+	private boolean isEmployeePresent() {
 		return new Random().nextBoolean();
 	}
 
-	private static String getEmployeeType() {
-		String[] arr = { "Full-time", "Part-time" };
-		return arr[new Random().nextInt(arr.length)];
+	public static void main(String[] args) {
+
+		EmployeeWageProblem jioMart = new EmployeeWageProblem("JIO Mart", 160, 20, 100);
+		EmployeeWageProblem bigBasket = new EmployeeWageProblem("Big Basket", 280, 22, 110);
+
+		System.out.println("Monthly wage for an employee of company " + jioMart.getCompanyName() + " is "
+				+ jioMart.getWagesForMonth());
+		System.out.println("Monthly wage for an employee of company " + bigBasket.getCompanyName() + " is "
+				+ bigBasket.getWagesForMonth());
 	}
 }
