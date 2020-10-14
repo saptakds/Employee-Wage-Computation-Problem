67b652e7d5834a92049ac57de7d5fc30108ba129
saptakds <saptakds@gmail.com>
2020-10-14 20:57:53 +0530

    Added: Ability to save total wage

--- src/com/saptak/employeewage/EmpWageBuilder.java
+++ src/com/saptak/employeewage/EmpWageBuilder.java
@@ -2,26 +2,24 @@ package com.saptak.employeewage;
 
 import java.util.Random;
 
-public class EmployeeWageProblem {
+public class EmpWageBuilder {
 
 	private static final int FULL_DAY_WORKING_HOURS = 8;
 	private final String companyName;
 	private final int dailyWage;
 	private final int workingDaysPerMonth;
 	private final int workingHoursPerMonth;
+	private long monthlyWage;
 
-	public EmployeeWageProblem(String companyName, int dailyWage, int workingDaysPerMonth, int workingHoursPerMonth) {
+	public EmpWageBuilder(String companyName, int dailyWage, int workingDaysPerMonth, int workingHoursPerMonth) {
 		this.companyName = companyName;
 		this.dailyWage = dailyWage;
 		this.workingDaysPerMonth = workingDaysPerMonth;
 		this.workingHoursPerMonth = workingHoursPerMonth;
+		calcWagesForMonth();
 	}
 
-	public String getCompanyName() {
-		return companyName;
-	}
-
-	public long getWagesForMonth() {
+	public void calcWagesForMonth() {
 		long wages = 0;
 		int workingHours = 0, workingDays = 0;
 		while (workingDays < workingDaysPerMonth && workingHours < workingHoursPerMonth) {
@@ -31,23 +29,27 @@ public class EmployeeWageProblem {
 				wages += dailyWage;
 			}
 		}
-		return wages;
+		monthlyWage = wages;
 	}
 
 	private boolean isEmployeePresent() {
 		return new Random().nextBoolean();
 	}
 
+	@Override
+	public String toString() {
+		return "Monthly wage for an employee of company " + companyName + " is " + monthlyWage;
+	}
+
 	public static void main(String[] args) {
-		
-		//Use case for git
 
-		EmployeeWageProblem jioMart = new EmployeeWageProblem("JIO Mart", 160, 20, 100);
-		EmployeeWageProblem bigBasket = new EmployeeWageProblem("Big Basket", 280, 22, 110);
+		EmpWageBuilder jioMart = new EmpWageBuilder("JIO Mart", 160, 20, 100);
+		EmpWageBuilder bigBasket = new EmpWageBuilder("Big Basket", 280, 22, 110);
 
-		System.out.println("Monthly wage for an employee of company " + jioMart.getCompanyName() + " is "
-				+ jioMart.getWagesForMonth());
-		System.out.println("Monthly wage for an employee of company " + bigBasket.getCompanyName() + " is "
-				+ bigBasket.getWagesForMonth());
+		jioMart.calcWagesForMonth();
+		bigBasket.calcWagesForMonth();
+
+		System.out.println(jioMart);
+		System.out.println(bigBasket);
 	}
 }
