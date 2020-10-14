cf5e50f7a40fbbf408447fd2e61adcfbbd7d1249
saptakds <saptakds@gmail.com>
2020-10-14 21:26:15 +0530

    Added: Ability to manage multiple companies through one EmpWageBuilder

---
+++ src/com/saptak/employeewage/CompanyEmpWage.java
@@ -0,0 +1,44 @@
+package com.saptak.employeewage;
+
+import java.util.Random;
+
+public class CompanyEmpWage {
+
+	private static final int FULL_DAY_WORKING_HOURS = 8;
+	private final String companyName;
+	private final int dailyWage;
+	private final int workingDaysPerMonth;
+	private final int workingHoursPerMonth;
+	private long monthlyWage;
+
+	public CompanyEmpWage(String companyName, int dailyWage, int workingDaysPerMonth, int workingHoursPerMonth) {
+		this.companyName = companyName;
+		this.dailyWage = dailyWage;
+		this.workingDaysPerMonth = workingDaysPerMonth;
+		this.workingHoursPerMonth = workingHoursPerMonth;
+		calcWagesForMonth();
+	}
+
+	public void calcWagesForMonth() {
+		long wages = 0;
+		int workingHours = 0, workingDays = 0;
+		while (workingDays < workingDaysPerMonth && workingHours < workingHoursPerMonth) {
+			workingDays++;
+			if (isEmployeePresent()) {
+				workingHours += FULL_DAY_WORKING_HOURS;
+				wages += dailyWage;
+			}
+		}
+		monthlyWage = wages;
+	}
+
+	private boolean isEmployeePresent() {
+		return new Random().nextBoolean();
+	}
+
+	@Override
+	public String toString() {
+		return "Monthly wage for an employee of company " + companyName + " is " + monthlyWage;
+	}
+
+}
--- src/com/saptak/employeewage/EmpWageBuilder.java
+++ src/com/saptak/employeewage/EmpWageBuilder.java
@@ -1,55 +1,21 @@
 package com.saptak.employeewage;
 
-import java.util.Random;
-
 public class EmpWageBuilder {
 
-	private static final int FULL_DAY_WORKING_HOURS = 8;
-	private final String companyName;
-	private final int dailyWage;
-	private final int workingDaysPerMonth;
-	private final int workingHoursPerMonth;
-	private long monthlyWage;
+	private CompanyEmpWage[] empWages;
 
-	public EmpWageBuilder(String companyName, int dailyWage, int workingDaysPerMonth, int workingHoursPerMonth) {
-		this.companyName = companyName;
-		this.dailyWage = dailyWage;
-		this.workingDaysPerMonth = workingDaysPerMonth;
-		this.workingHoursPerMonth = workingHoursPerMonth;
-		calcWagesForMonth();
-	}
-
-	public void calcWagesForMonth() {
-		long wages = 0;
-		int workingHours = 0, workingDays = 0;
-		while (workingDays < workingDaysPerMonth && workingHours < workingHoursPerMonth) {
-			workingDays++;
-			if (isEmployeePresent()) {
-				workingHours += FULL_DAY_WORKING_HOURS;
-				wages += dailyWage;
-			}
-		}
-		monthlyWage = wages;
-	}
-
-	private boolean isEmployeePresent() {
-		return new Random().nextBoolean();
-	}
-
-	@Override
-	public String toString() {
-		return "Monthly wage for an employee of company " + companyName + " is " + monthlyWage;
+	public EmpWageBuilder() {
+		empWages = new CompanyEmpWage[2];
 	}
 
 	public static void main(String[] args) {
 
-		EmpWageBuilder jioMart = new EmpWageBuilder("JIO Mart", 160, 20, 100);
-		EmpWageBuilder bigBasket = new EmpWageBuilder("Big Basket", 280, 22, 110);
+		EmpWageBuilder emps = new EmpWageBuilder();
+		emps.empWages[0] = new CompanyEmpWage("Big Basket", 280, 22, 110);
+		emps.empWages[1] = new CompanyEmpWage("JIO Mart", 200, 20, 100);
 
-		jioMart.calcWagesForMonth();
-		bigBasket.calcWagesForMonth();
-
-		System.out.println(jioMart);
-		System.out.println(bigBasket);
+		for (int i = 0; i < 2; i++) {
+			System.out.println(emps.empWages[i]);
+		}
 	}
 }
